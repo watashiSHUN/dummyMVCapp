89794725d635396a22e0616c356667b6080b59dc
ShunXian Cai <shucai@microsoft.com>
2017-03-21 23:47:10 -0700

    Add project files.

---
+++ dummyMVCapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dummyMVCapp.Startup))]
+namespace dummyMVCapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
